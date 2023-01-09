6974df5c05ed2bd0f2a4028db2c7eed74b967a4c
Aeternalis-Ingenium <aeternalis-ingenium@proton.me>
2023-01-09 14:38:15 +0100

    fix(save-and-load-models): Add extra code for saving and loading SKLearn model
    Both `save_model` and `load_model` can save and load `.h5` for TensorFlow model and `.pkl` for SKLearn model. The new source code has passed the test for both functions. Test can be found in `tests/test_model_versioniing.py`.

--- helpers/models.py
+++ helpers/models.py
@@ -1,44 +1,116 @@
+import enum
 import os
 import pathlib
-import shutil
+import pickle
 import tensorflow as tf
+import typing
 
 ROOT_DIR = pathlib.Path().parent.resolve()
-MODEL_DIR = ROOT_DIR / pathlib.Path("data/models/")
+MODEL_DIR = str(ROOT_DIR / pathlib.Path("data/models/"))
+
+
+def set_filepath(
+    base_dir: str,
+    model_dir_name: str,
+    model_fname: str,
+    extension: str
+) -> pathlib.Path:
+        return pathlib.Path(base_dir) / pathlib.Path(model_dir_name) / pathlib.Path(model_fname + f"{extension}")
 
 
 def save_model(
-    model: tf.keras.Model,
+    ml_library: typing.Literal["tensorflow", "scikit-learn"],
+    model: tf.keras.Model | typing.Any,
     model_name: str,
     model_version: int,
-    model_dir_version: int
+    model_dir_version: int,
+    base_dir: str = MODEL_DIR
 ) -> None:
-    fname = model_name + "_v_" + str(model_version) + ".h5"
-    dir_name = model_name + "_V_" + str(model_dir_version)
-    model_dir_path = MODEL_DIR / pathlib.Path(dir_name)
-    model_filepath = model_dir_path / pathlib.Path(fname)
+    model_dir_name = model_name + "_V_" + str(model_dir_version)
+    model_fname = model_name + "_v_" + str(model_version)
+    model_dir_path = pathlib.Path(base_dir) / pathlib.Path(model_dir_name)
 
-    print(f"Saving the new model {model_name} in {model_dir_path}. . .")
+    print(f"Saving the new model {model_name} in:\n{model_dir_path}")
 
-    if os.path.exists(path=model_dir_path):
-        model.save(filepath=model_filepath)
-    else:
-        os.mkdir(path=model_dir_path)
-        model.save(filepath=model_filepath)
+    if ml_library.lower() == "tensorflow":
 
-    print("The model is successfully saved:\n")
-    print(f"    - Model Name: {model_name}")
-    print(f"    - Model Saving Path: {model_filepath}")
+        model_filepath = set_filepath(
+            base_dir=base_dir,
+            model_dir_name=model_dir_name,
+            model_fname=model_fname,
+            extension=".h5"
+        )
 
-    return
+        if os.path.exists(path=model_dir_path):
+            model.save(filepath=model_filepath)
+        else:
+            os.mkdir(path=model_dir_path)
+            model.save(filepath=model_filepath)
+
+        print("The model is successfully saved:\n")
+        print(f"    - Model Name: {model_name}")
+        print(f"    - Model Saving Path: {model_filepath}")
+
+        return
+
+    elif ml_library.lower() == "scikit-learn":
+        model_filepath = set_filepath(
+            base_dir=base_dir,
+            model_dir_name=model_dir_name,
+            model_fname=model_fname,
+            extension=".pkl"
+        )
+
+        if os.path.exists(path=model_dir_path):
+            pickle.dump(model, open(file=model_filepath, mode="wb"))
+        else:
+            os.mkdir(path=model_dir_path)
+            pickle.dump(model, open(file=model_filepath, mode="wb"))   # type: ignore
+
+        print("The model is successfully saved:\n")
+        print(f"    - Model Name: {model_name}")
+        print(f"    - Model Saving Path: {model_filepath}")
+
+        return
+
+    raise Exception("Uuppss.. Nothing happens. Please re-check all your arguments!")
 
 
 def load_model(
-    model_dir_name: str,
+    ml_library: typing.Literal["tensorflow", "scikit-learn"],
     model_name: str,
+    model_version: int,
+    model_dir_version: int,
+    base_dir: str = MODEL_DIR,
     custom_objects: dict | None = None
 ):
-    return tf.keras.models.load_model(
-        filepath=MODEL_DIR / pathlib.Path(model_dir_name) / pathlib.Path(model_name + ".h5"),
-        custom_objects=custom_objects
-    )
+    model_dir_name = model_name + "_V_" + str(model_dir_version)
+    model_fname = model_name + "_v_" + str(model_version)
+    model_dir_path = pathlib.Path(base_dir) / pathlib.Path(model_dir_name)
+
+    print(f"Loading {ml_library} model from:\n{model_dir_path}")
+
+    if ml_library.lower() == "tensorflow":
+        return tf.keras.models.load_model(
+            filepath=set_filepath(
+                base_dir=base_dir,
+                model_dir_name=model_dir_name,
+                model_fname=model_fname,
+                extension=".h5"
+            ),
+            custom_objects=custom_objects
+        )
+
+    elif ml_library.lower() == "scikit-learn":
+        return pickle.load(file=open(
+            file=set_filepath(
+                base_dir=base_dir,
+                model_dir_name=model_dir_name,
+                model_fname=model_fname,
+                extension=".pkl"
+                ),
+                mode="rb"
+            )
+        )
+
+    raise Exception("Uuppss.. Nothing happens. Please re-check all your arguments!")
--- tests/test_model_versioning.py
+++ tests/test_model_versioning.py
@@ -2,18 +2,15 @@ import shutil
 import pathlib
 import unittest
 
+from sklearn import tree
 from keras.models import Sequential
 from keras.layers import (
     Input,
-    Dense,
-    Activation,
-    Conv2D,
-    MaxPooling2D,
-    Flatten
+    Dense
 ) 
 
 from data.dataset import Dataset
-from helpers.models import save_model, load_model
+from helpers.models import MODEL_DIR, save_model, load_model
 
 
 class TestModelVersioning(unittest.TestCase):
@@ -22,59 +19,117 @@ class TestModelVersioning(unittest.TestCase):
     """
 
     def setUp(self):        
-        self.model_name = "Sequential_CNN_Test_Model"
+        self.tf_model_name = "Sequential_CNN_TEST_Model"
+        self.sk_model_name = "DTC_CreiterionGini_TEST_MODEL"
         self.model_version = 1
         self.model_dir_version = 1
-        self.model = Sequential(name=self.model_name)
-        self.dataset = Dataset()
+        self.tf_model = Sequential(name=self.tf_model_name)
+        self.sk_model = tree.DecisionTreeClassifier()
 
-        self.model.add(Input(shape=(4,)))
-        self.model.add(Dense(2, activation="relu"))
+        self.tf_model.add(Input(shape=(4,)))
+        self.tf_model.add(Dense(2, activation="relu"))
+        self.tf_model.compile(loss="binary_crossentropy", optimizer="adam", run_eagerly=True)
 
 
-    def test_save_model(self):
+    def test_save_sk_model(self):
         """
             test interaction between dataset download and check for downloading
         """
         save_model(
-            model=self.model,
-            model_name=self.model_name,
-            model_version=self.model_version,
-            model_dir_version=self.model_dir_version
+            ml_library="scikit-learn",
+            model=self.sk_model,
+            model_name=self.sk_model_name,
+            model_version=1,
+            model_dir_version=1
         )
 
-        assert pathlib.Path(f"data/models/{self.model_name}_V_{self.model_dir_version}/{self.model_name}_v_{self.model_version}.h5").is_file() == True
+        assert pathlib.Path(f"{MODEL_DIR}/{self.sk_model_name}_V_{self.model_dir_version}/{self.sk_model_name}_v_{self.model_version}.pkl").is_file() == True
 
 
-    def test_save_same_model(self):
+    def test_save_tf_model(self):
+        """
+            test interaction between dataset download and check for downloading
+        """
         save_model(
-            model=self.model,
-            model_name=self.model_name,
-            model_version=self.model_version,
-            model_dir_version=self.model_dir_version
+            ml_library="tensorflow",
+            model=self.tf_model,
+            model_name=self.tf_model_name,
+            model_version=1,
+            model_dir_version=1
         )
-        assert pathlib.Path(f"data/models/{self.model_name}_V_{self.model_dir_version}/{self.model_name}_v_{self.model_version}.h5").is_file() == True
+
+        assert pathlib.Path(f"{MODEL_DIR}/{self.tf_model_name}_V_{self.model_dir_version}/{self.tf_model_name}_v_{self.model_version}.h5").is_file() == True
 
 
-    def test_load_model(self):
+    def test_overwriting_same_sk_model_with_same_filename(self):
+        save_model(
+            ml_library="scikit-learn",
+            model=self.sk_model,
+            model_name=self.sk_model_name,
+            model_version=1,
+            model_dir_version=1
+        )
+        save_model(
+            ml_library="scikit-learn",
+            model=self.sk_model,
+            model_name=self.sk_model_name,
+            model_version=1,
+            model_dir_version=1
+        )
+
+        model_files = [model_file.name for model_file in pathlib.Path(f"{MODEL_DIR}/{self.sk_model_name}_V_{self.model_dir_version}").iterdir()]
+
+        assert len(model_files) == 1
+
+
+    def test_load_sk_model(self):
         """
             test automatic download of get_original_data
         """
         save_model(
-            model=self.model,
-            model_name=self.model_name,
-            model_version=self.model_version,
-            model_dir_version=self.model_dir_version
+            ml_library="scikit-learn",
+            model=self.sk_model,
+            model_name=self.sk_model_name,
+            model_version=1,
+            model_dir_version=1
         )
 
-        test_model = load_model(
-            model_dir_name="Sequential_CNN_Test_Model_V_1",
-            model_name="Sequential_CNN_Test_Model_v_1"
+        sk_loaded_model = load_model(
+            ml_library="scikit-learn",
+            model_name=self.sk_model_name,
+            model_version=1,
+            model_dir_version=1
         )
 
-        assert test_model.name == self.model_name  # type: ignore
-        assert isinstance(test_model, Sequential)
+        assert isinstance(sk_loaded_model, tree.BaseDecisionTree)
+        assert sk_loaded_model.get_params() == self.sk_model.get_params()
+
+
+    def test_load_tf_model(self):
+        """
+            test automatic download of get_original_data
+        """
+        save_model(
+            ml_library="tensorflow",
+            model=self.tf_model,
+            model_name=self.tf_model_name,
+            model_version=1,
+            model_dir_version=1
+        )
+
+        tf_loaded_model = load_model(
+            ml_library="tensorflow",
+            model_name=self.tf_model_name,
+            model_version=1,
+            model_dir_version=1
+        )
+
+        assert isinstance(tf_loaded_model, Sequential)
+        assert tf_loaded_model.name == self.tf_model_name
 
 
     def tearDown(self):
-        shutil.rmtree(path=f"data/models/{self.model_name}_V_{self.model_dir_version}")
+        if pathlib.Path(f"{MODEL_DIR}/{self.sk_model_name}_V_{self.model_dir_version}/{self.sk_model_name}_v_{self.model_version}.pkl").is_file():
+            shutil.rmtree(path=pathlib.Path(f"{MODEL_DIR}/{self.sk_model_name}_V_{self.model_dir_version}"))
+        if pathlib.Path(f"{MODEL_DIR}/{self.tf_model_name}_V_{self.model_dir_version}/{self.tf_model_name}_v_{self.model_version}.h5").is_file():
+            shutil.rmtree(path=pathlib.Path(f"{MODEL_DIR}/{self.tf_model_name}_V_{self.model_dir_version}"))
